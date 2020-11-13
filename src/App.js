a9bd28b3c2ccaa5981239f1fa6ac24d65e9353a4
GoeJelson <45436010+GoeJelson@users.noreply.github.com>
2020-11-13 12:59:47 -0600

    initial bulk commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Shopping Kart Site Coming Soon!!
+        </p>
+        <a
+          className="App-link"
+          href="https://azure-landing-jn.azurewebsites.net/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Back to landing page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
